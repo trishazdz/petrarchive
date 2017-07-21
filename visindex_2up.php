054cafd56de99ea713e1aeb88ef03d0396514fd6
Isabella Magni <isamagni@Isabellas-MacBook-Pro.local>
2017-07-21 15:39:03 -0400

    Updated visual indexes

--- visindex_2up.php
+++ visindex_2up.php
@@ -137,16 +137,16 @@
 			<a href="content/c024v-c025v.xml#c025r"><img alt="vat. lat. 3195, c. 25v" class="visindex-verso" src="images/visindex/c025v.svg" id="c025v" /></a>  <a href="content/c026r.xml"><img alt="vat. lat. 3195, c. 26r" class="visindex-recto" src="images/visindex/c026r.svg" id="c026r" /></a>  
 		</div>
 		<div>
-			<img alt="vat. lat. 3195, c. 26v" class="visindex-verso" src="images/visindex/c026v.svg" id="c026v" />  <a href="content/c027r.xml"><img alt="vat. lat. 3195, c. 27r" class="visindex-recto" src="images/visindex/c027r.svg" id="c027r" /></a>  
+			<img alt="vat. lat. 3195, c. 26v" class="visindex-verso" src="images/visindex/c026v.svg" id="c026v" />  <a href="content/c027r-c029v.xml#c027r"><img alt="vat. lat. 3195, c. 27r" class="visindex-recto" src="images/visindex/c027r.svg" id="c027r" /></a>  
 		</div>
 		<div>
-			<img alt="vat. lat. 3195, c. 27v" class="visindex-verso" src="images/visindex/c027v.svg" id="c027v" />  <a href="content/c028r-c029v.xml#c028r"><img alt="vat. lat. 3195, c. 28r" class="visindex-recto" src="images/visindex/c028r.svg" id="c028r" /></a>  
+			<a href="content/c027r-c029v.xml#c027v"><img alt="vat. lat. 3195, c. 27v" class="visindex-verso" src="images/visindex/c027v.svg" id="c027v" /></a>  <a href="content/c027r-c029v.xml#c028r"><img alt="vat. lat. 3195, c. 28r" class="visindex-recto" src="images/visindex/c028r.svg" id="c028r" /></a>  
 		</div>
 		<div>
-			<a href="content/c028r-c029v.xml#c028v"><img alt="vat. lat. 3195, c. 28v" class="visindex-verso" src="images/visindex/c028v.svg" id="c028v" /></a>  <a href="content/c028r-c029v.xml#c029r"><img alt="vat. lat. 3195, c. 29r" class="visindex-recto" src="images/visindex/c029r.svg" id="c029r" /></a>  
+			<a href="content/c027r-c029v.xml#c028v"><img alt="vat. lat. 3195, c. 28v" class="visindex-verso" src="images/visindex/c028v.svg" id="c028v" /></a>  <a href="content/c027r-c029v.xml#c029r"><img alt="vat. lat. 3195, c. 29r" class="visindex-recto" src="images/visindex/c029r.svg" id="c029r" /></a>  
 		</div>
 		<div>
-			<a href="content/c028r-c029v.xml#c029v"><img alt="vat. lat. 3195, c. 29v" class="visindex-verso" src="images/visindex/c029v.svg" id="c029v" />  <a href="content/c030r.xml"><img alt="vat. lat. 3195, c. 30r" class="visindex-recto" src="images/visindex/c030r.svg" id="c030r" /></a>  
+			<a href="content/c027r-c029v.xml#c029v"><img alt="vat. lat. 3195, c. 29v" class="visindex-verso" src="images/visindex/c029v.svg" id="c029v" />  <a href="content/c030r.xml"><img alt="vat. lat. 3195, c. 30r" class="visindex-recto" src="images/visindex/c030r.svg" id="c030r" /></a>  
 		</div>
 		<div>
 			<img alt="vat. lat. 3195, c. 30v" class="visindex-verso" src="images/visindex/c030v.svg" id="c030v" />  <img alt="vat. lat. 3195, c. 31r" class="visindex-recto" src="images/visindex/c031r.svg" id="c031r" />  

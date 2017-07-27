18141a4a1988e2bf7192a9c3321ea69fe17f8bca
Isabella Magni <isamagni@Isabellas-MacBook-Pro.local>
2017-07-27 11:13:13 -0400

    Added links to c32r visual indexes

--- visindex_2up.php
+++ visindex_2up.php
@@ -152,7 +152,7 @@
 			<a href="content/c030v-c031r.xml#c030v"><img alt="vat. lat. 3195, c. 30v" class="visindex-verso" src="images/visindex/c030v.svg" id="c030v" /></a>  <a href="content/c030v-c031r.xml#c031r"><img alt="vat. lat. 3195, c. 31r" class="visindex-recto" src="images/visindex/c031r.svg" id="c031r" /></a>  
 		</div>
 		<div>
-			<a href="content/c031v.xml"><img alt="vat. lat. 3195, c. 31v" class="visindex-verso" src="images/visindex/c031v.svg" id="c031v" /></a>  <img alt="vat. lat. 3195, c. 32r" class="visindex-recto" src="images/visindex/c032r.svg" id="c032r" />  
+			<a href="content/c031v.xml"><img alt="vat. lat. 3195, c. 31v" class="visindex-verso" src="images/visindex/c031v.svg" id="c031v" /></a>  <a href="content/c032r.xml"><img alt="vat. lat. 3195, c. 32r" class="visindex-recto" src="images/visindex/c032r.svg" id="c032r" /></a>  
 		</div>
 		<div>
 			<a href="content/c032v.xml"><img alt="vat. lat. 3195, c. 32v" class="visindex-verso" src="images/visindex/c032v.svg" id="c032v" /></a>  <img alt="vat. lat. 3195, c. 33r" class="visindex-recto" src="images/visindex/c033r.svg" id="c033r" />  

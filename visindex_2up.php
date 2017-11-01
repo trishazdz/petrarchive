1f1cd0c89c2957eb4f6caf423e9aa96a9a5a915a
Isabella Magni <isamagni@Isabellas-MacBook-Pro.local>
2017-11-01 15:03:31 -0500

    Updated visual indexes to include 33v

--- visindex_2up.php
+++ visindex_2up.php
@@ -158,7 +158,7 @@
 			<a href="content/c032v.xml"><img alt="vat. lat. 3195, c. 32v" class="visindex-verso" src="images/visindex/c032v.svg" id="c032v" /></a>  <a href="content/c033r.xml"><img alt="vat. lat. 3195, c. 33r" class="visindex-recto" src="images/visindex/c033r.svg" id="c033r" /></a>  
 		</div>
 		<div>
-			<img alt="vat. lat. 3195, c. 33v" class="visindex-verso" src="images/visindex/c033v.svg" id="c033v" />  <img alt="vat. lat. 3195, c. 34r" class="visindex-recto" src="images/visindex/c034r.svg" id="c034r" />  
+			<a href="content/c033v.xml"><img alt="vat. lat. 3195, c. 33v" class="visindex-verso" src="images/visindex/c033v.svg" id="c033v" /></a>  <img alt="vat. lat. 3195, c. 34r" class="visindex-recto" src="images/visindex/c034r.svg" id="c034r" />  
 		</div>
 		<div>
 			<img alt="vat. lat. 3195, c. 34v" class="visindex-verso" src="images/visindex/c034v.svg" id="c034v" />  <img alt="vat. lat. 3195, c. 35r" class="visindex-recto" src="images/visindex/c035r.svg" id="c035r" />  
